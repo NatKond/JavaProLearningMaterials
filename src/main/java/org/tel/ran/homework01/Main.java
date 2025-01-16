81d5b3138db575bf1edce191f948dc6260df69b1
NatKond <natzubova@gmail.com>
2025-01-16 14:25:22 +0100

    Changes to FibonacciSequence in homework01.

--- src/main/java/org/tel/ran/homework01/Main.java
+++ src/main/java/org/tel/ran/homework01/Main.java
@@ -5,17 +5,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        FibonacciSequence fs1;
+        System.out.print("Enter the length of the Fibonacci sequence: ");
         String length = scanner.nextLine();
 
-        while (!length.isEmpty()){
-            fs1 = new FibonacciSequence(Integer.parseInt(length));
-            System.out.println("fs1.getSequenceFor() = " + Arrays.toString(fs1.getSequenceFor()));
-            System.out.println("fs1.getSequenceWhile() = " + Arrays.toString(fs1.getSequenceWhile()));
-            System.out.println("fs1.getSequenceDoWhile() = " + Arrays.toString(fs1.getSequenceDoWhile()));
-            length = scanner.nextLine();
-        }
+        FibonacciSequence fs1 = new FibonacciSequence(Integer.parseInt(length));
+        System.out.println("fs1.getSequenceFor() = " + Arrays.toString(fs1.getSequenceFor()));
+        System.out.println("fs1.getSequenceWhile() = " + Arrays.toString(fs1.getSequenceWhile()));
+        System.out.println("fs1.getSequenceDoWhile() = " + Arrays.toString(fs1.getSequenceDoWhile()));
 
     }
 }
