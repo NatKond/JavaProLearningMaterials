569ed5eae700c21f66e51ff523c55d97faaeb942
NatKond <natzubova@gmail.com>
2025-02-20 16:41:17 +0100

    Changes to homework07.

--- src/main/java/org/tel/ran/homework07/Main.java
+++ src/main/java/org/tel/ran/homework07/Main.java
@@ -3,12 +3,21 @@ package org.tel.ran.homework07;
 import java.util.*;
 
 import static org.tel.ran.textFormatting.*;
+
+/**
+ * Программа принимает на вход текст, произвольной длины, обрабатывает его и выводит все слова, которые встречаются в тексте (без дубликатов) группируя их по первой букве в алфавитном порядке.
+ * Знаки препинания, кавычки, скобки (кроме апострофа) игнорируются, числа также игнорируются.
+ * Регистр заглавных букв не учитывается. Подсчитать количество вхождений.
+ */
+
 /*
-It's a sunny day, but Jack's not happy. He doesn't know why. Maybe it's because he's forgotten something important. Let's see—oh, it's his best friend's birthday! He'd better hurry!
+It's a sunny day, but Jack's not happy. He doesn't know why. Maybe it's because he's forgotten something important. Let's see —oh, it's his best friend's birthday! He'd better hurry!
 
  1999, a small town had only 2 schools and 3 grocery stores. Now, in 2024, it has 10 schools, 15 grocery stores, and over 50 restaurants. The population grew from 5,000 to 20,000 in just 25 years!
 
 Apple, apple, apple! I love apples. Red apples, green apples, and yellow apples. Apples are great for pies, apple juice, apple sauce, and apple snacks. Who doesn't love apples?
+
+Once upon a time a Wolf was lapping at a spring on a hillside, when, looking up, what should he see but a Lamb just beginning to drink a little lower down.
  */
 
 public class Main {
@@ -35,8 +44,7 @@ public class Main {
     }
 
     private static Map<String, Integer> getWordsCountMap(String string) {
-        string = string.replaceAll("[^\\p{L}' ]", "").trim().replaceAll(" {2}", " ");
-        String[] strings = string.toLowerCase().split("\\s");
+        String[] strings = string.replaceAll("[^\\p{L}' ]", "").trim().toLowerCase().split("\\s+");
         Map<String, Integer> output = new TreeMap<>(Comparator.naturalOrder());
         for (String str : strings) {
             if (output.containsKey(str)) {
